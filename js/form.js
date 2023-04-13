39f58d1710049377b08a17c97357e9890e2ef08b
Enmanuel Bayen <121642946+enmanuelbayen@users.noreply.github.com>
2023-04-13 15:47:15 -0400

    hide

--- js/form.js
+++ js/form.js
@@ -3,7 +3,7 @@ const email = document.getElementById('email-contact');
 const emailError = document.querySelector('.email-error-msg');
 
 form.addEventListener('submit', (event) => {
-  if (email.value !== email.value.toLowerCase()) {
+  if (email.value === email.value.toLowerCase()) {
     emailError.textContent = 'Email need to be on lowercase';
     emailError.style.display = 'block';
     event.preventDefault();
